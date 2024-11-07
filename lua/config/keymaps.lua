004d783bcb6172be2d49edc64b2aff16b15e89c7
ykai <me@ykai.cc>
2024-11-08 02:10:32 +0800

    add my config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map('i', '<c-a>', '<home>')
+map('i', '<c-e>', '<end>')
+map('i', '<c-f>', '<right>')
+map('i', '<c-b>', '<left>')
+map('i', '<m-f>', '<c-right>')
+map('i', '<m-b>', '<c-left>')
+map('i', '<m-right>', '<c-right>')
+map('i', '<m-left>', '<c-left>')
+map('c', '<c-a>', '<home>')
+map('c', '<c-e>', '<end>')
+map('c', '<c-f>', '<right>')
+map('c', '<c-b>', '<left>')
+map('c', '<m-f>', '<c-right>')
+map('c', '<m-b>', '<c-left>')
+map('c', '<m-right>', '<c-right>')
+map('c', '<m-left>', '<c-left>')
---
+++ lua/plugins/config.lua
@@ -0,0 +1,67 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
+        }
+      }
+    }
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    -- enabled = false
+  },
+
+  {
+    "folke/noice.nvim",
+    opts = {
+      -- cmdline = {
+      --   enabled = false,
+      -- },
+    },
+  }
+}
