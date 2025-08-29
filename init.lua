4c7d8156ea700b1d716af87671892711f6c6bae4
dangyingkai <dangyingkai@bytedance.com>
2025-08-29 13:23:43 +0800

    add clangd

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- vim.lsp.config["ktls"] = {
+--   cmd = {
+--     "/Users/bytedance/Downloads/kotlin-0.252.17811/kotlin-lsp.sh",
+--     "--stdio"
+--   },
+--   filetypes = { "kotlin" },
+-- }
+-- 
+-- vim.lsp.enable("ktls")
